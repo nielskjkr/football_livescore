96c1c61bdbf21a1749e99e438875b07e3f3916ca
nielskjkr <59951829+nielskjkr@users.noreply.github.com>
2021-10-05 20:43:13 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,4 @@
 """
-The Car Wash binary sensor.
+The football live score binary sensor.
 
-For more details about this platform, please refer to the documentation at
-https://github.com/Limych/ha-car_wash/
 """
